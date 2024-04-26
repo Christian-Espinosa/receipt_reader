c612574afc1bad37c1c599c47699a331da37587a
CHRISTIAN ESPINOSA REBOREDO <christian.espinosa@sdggroup.local>
2024-04-26 15:03:53 +0200

    Progreso tesseract

--- test_tesseract.py
+++ test_tesseract.py
@@ -1,9 +1,45 @@
 import cv2
 import pytesseract
+import paths
+from pdf2image import convert_from_path
+import numpy as np
+import matplotlib.pyplot as plt
+from PyPDF2 import PdfFileReader
 
-#create a hello word
-    # Load the image
-    image = cv2.imread('path/to/your/image.jpg')
+def read_pdf_files(path):
+    pdf_files = []
+
+    with open(path, 'rb') as file:
+        for line in file:
+            pdf_files.append(line.strip())
+
+
+    return pdf_files
+
+def read_pdf_pages(pdf_file):
+    pdf_images = []
+    pages = convert_from_path(pdf_file)
+    for page in pages:
+        image = np.array(page)
+        pdf_images.append(image)
+    return pdf_images
+
+def main():
+
+    l_pdfs = read_pdf_files(paths.img_path)
+    l_imgs = read_pdf_pages(l_pdfs)
+
+    # Create a figure to display the images
+    fig, axes = plt.subplots(nrows=len(l_imgs), ncols=1)
+
+    # Iterate over the images and display them in the figure
+    for i, img in enumerate(l_imgs):
+        axes[i].imshow(img)
+        axes[i].axis('off')
+
+    # Show the figure
+    plt.show()
+    
 
     # Crop the image (replace the values with your desired coordinates)
     cropped_image = image[y_start:y_end, x_start:x_end]
@@ -19,3 +55,6 @@ import pytesseract
 
     # Print the extracted text
     print(text)
+    
+if __name__ == "__main__":
+    main()
